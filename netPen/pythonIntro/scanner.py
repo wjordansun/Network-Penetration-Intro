142cb05ec508d8f169092f02c5cfebe9f6d4b7fb
root <root@kali>
2021-04-03 20:58:45 -0400

    updated scanner.py

--- netPen/pythonIntro/scanner.py
+++ netPen/pythonIntro/scanner.py
@@ -12,4 +12,31 @@ else:
     print("Syntax: python3 scanner.py <ip>")
     sys.exit()
 
+#Add a pretty banner
+print("-" * 50)
+print("Scanning target " + target)
+print("Time started: " + str(datetime.now()))
+print("-" * 50)
+
+try:
+    for port in range(50, 85):
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #IPv4, port
+        socket.setdefaulttimeout(10) #is a float
+        result = s.connect_ex((target, port)) #returns error indicator: 0 if no error
+        print("Checking port {}".format(port))
+        if result == 0:
+            print("Port {} is open".format(port))
+        s.close()
+except KeyboardInterrupt:
+    print("\nExiting program.")
+    sys.exit()
+
+except socket.gaierror:
+    print("Hostname could not be resolved.")
+    sys.exit()
+
+except socket.error:
+    print("Couldn't connect to server.")
+    sys.exit()
+
 
